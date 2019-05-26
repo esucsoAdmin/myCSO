016fd570930aee118411ef0392b9bb5f1478e5aa
xXiPrimusXx <a.fetter97@gmail.com>
2019-05-26 15:27:31 -0400

    Initializing the repo for our cross-platform application for the CSO. In this first commit the following has been done: Default launcher icon has been changed to the CSO logo and a navigation bar at the bottom has been added along with sample pages for each tab.

--- App.js
+++ App.js
@@ -117,6 +117,7 @@ export default createAppContainer(
                 activeBackgroundColor: 'black',
                 inactiveBackgroundColor: 'black',
             },
+            initialRouteName: 'Home',
         }
     )
 );
